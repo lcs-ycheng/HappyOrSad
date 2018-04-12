c6452e7f019686719a1a38dd37f54aff2b2799dc
Yaming Cheng <ycheng@lcs.on.ca>
2018-04-12 13:48:19 -0400

    Finish happy and sad face

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -11,7 +11,43 @@ import Foundation
 // Get the user input
 var rawInput = readLine()
 
+//Make sure the input was given (create a String from the String?)
+guard let input = rawInput else {
+    //error
+    exit(9)
+}
+
 // Print out the input provided
 print("You said:")
-print(rawInput)
+//print(rawInput) //optional String
+print(input) //non-optional
 
+//Process - inspect each character
+var happyCount = 0
+var sadCount = 0
+for individualCharacter in input {
+    //Debug
+    print(individualCharacter)
+    
+    //Categorize the character
+    switch individualCharacter {
+    case "😃", "😄","🙂","😊" :
+        happyCount += 1
+    case "🙁","☹️","😔","😕":
+        sadCount += 1
+    default:
+        break //do nothing
+    }
+}
+//Output
+//Tell the user "happy", "sad" or "unsure" based on the counts above
+
+if happyCount == sadCount{
+    print("unsure")
+}else if happyCount > sadCount{
+    print("happy")
+} else if sadCount > happyCount{
+    print("sad")
+}else if happyCount == 0 && sadCount == 0{
+    print("none")
+}
